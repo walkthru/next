9e876de396fcb9bbf2172358a13c85d0d412821f
Anthony Gore <anthony@vuejsdevelopers.com>
2022-06-09 11:08:26 +1000

    Fixing iphone height issue

--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,7 @@ import Image from 'next/image'
 
 function Layout({ children }) {
   return (
-    <div
-      className="h-screen flex flex-col"
-    >
+    <div className="h-screen flex flex-col">
       <header className="flex-none bg-gray-700 p-4 md:px-8 py-2 flex justify-between align-middle">
         <div className="flex gap-2 items-center">
           <Image alt="Walkthru" src="/logo.png" width={25} height={25} />
