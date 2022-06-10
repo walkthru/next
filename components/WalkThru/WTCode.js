df445a3ecf0464fa8cd9f06063106eaaf1350135
Anthony Gore <anthony@vuejsdevelopers.com>
2022-06-10 14:58:08 +1000

    Fixing line numbers

--- components/WalkThru/WTCode.js
+++ components/WalkThru/WTCode.js
@@ -13,54 +13,62 @@ const Pre = styled.pre`
   border-radius: 0.25rem;
 `
 
-const Line = styled.div`
+const Code = styled.code`
+  display: flex;
+  flex-direction: row;
+  overflow-x: hidden;
+`
+
+const LineNumbers = styled.div`
+  overflow: scroll;
+  flex-shrink: 0;
+`
+
+const Lines = styled.div`
+  flex-grow: 1;
+`
+
+const Highlightable = styled.div`
   opacity: ${(props) => (props.highlighted ? '1;' : '0.5;')}
   -webkit-transition: opacity 100ms linear;
   -ms-transition: opacity 100ms linear;
   transition: opacity 100ms linear;
+  ${Pre}:active & {
+    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
+  }
+  ${Pre}:hover & {
+    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
+  }
+  ${Pre}.no-highlight & {
+    opacity: 1;
+  }
+`
+
+const LineNumber = styled(Highlightable)`
+  display: inline-flex;
+  justify-content: center;
+  width: 3rem;
+  flex-shrink: 0;
+  background-color: ${(props) =>
+    props.highlighted ? '#100d0b;' : 'rgb(28 25 23);'};
+`
+
+const Line = styled(Highlightable)`
   display: flex;
   background-color: ${(props) =>
     props.highlighted ? '#100d0b;' : 'rgb(28 25 23);'}
   &:hover {
     background-color: #292524;
   }
-  ${Pre}:active & {
-    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
-  }
-  ${Pre}:hover & {
-    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
-  }
-  ${Pre}.no-highlight & {
-    opacity: 1;
-  }
-`
-
-const Token = styled.span`
-  opacity: ${(props) => (props.highlighted ? '1;' : '0.5;')}
-  -webkit-transition: opacity 100ms linear;
-  -ms-transition: opacity 100ms linear;
-  transition: opacity 100ms linear;
-  ${Pre}:active & {
-    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
-  }
-  ${Pre}:hover & {
-    opacity: ${(props) => (props.highlighted ? '1;' : '0.8;')}
-  }
-  ${Pre}.no-highlight & {
-    opacity: 1;
-  }
-`
-
-const LineNo = styled.div`
-  display: inline-flex;
-  justify-content: center;
-  width: 3rem;
-  flex-shrink: 0;
 `
 
 const LineContent = styled.div`
   white-space: ${(props) => (props.wrap ? 'pre-wrap;' : 'pre;')}
   word-break: ${(props) => (props.wrap ? 'break-word;' : 'normal;')}
+`
+
+const Token = styled(Highlightable)`
+  display: inline-block;
 `
 
 function getHighlightedLines(focus) {
@@ -138,33 +146,51 @@ function WTCode({ files, step, sameFile, config }) {
                 highlightedLines.length ? '' : 'no-highlight'
               }`}
             >
-              <code>
-                {tokens.map((line, i) => {
-                  const lineProps = getLineProps({ line, key: i })
-                  if (highlightedLines.indexOf(i + 1) > -1) {
-                    lineProps.highlighted = true
-                  }
-                  return (
-                    <Line {...lineProps} key={i.toString()}>
-                      <LineNo
-                        className="__line-no"
-                        key={`number-${i.toString()}`}
+              <Code>
+                <LineNumbers>
+                  {tokens.map((line, i) => {
+                    const lineProps = getLineProps({ line, key: i })
+                    if (highlightedLines.indexOf(i + 1) > -1) {
+                      lineProps.highlighted = true
+                    }
+                    return (
+                      <div
+                        style={lineProps.style}
+                        key={`number-wrapper-${i.toString()}`}
                       >
-                        {i + 1}
-                      </LineNo>
-                      <LineContent key={`content-${i.toString()}`}>
-                        {line.map((token, key) => {
-                          const tokenProps = getTokenProps({ token, key })
-                          if (highlightedLines.indexOf(i + 1) > -1) {
-                            tokenProps.highlighted = true
-                          }
-                          return <Token {...tokenProps} key={`${i}-${key}`} />
-                        })}
-                      </LineContent>
-                    </Line>
-                  )
-                })}
-              </code>
+                        <LineNumber
+                          highlighted={lineProps.highlighted}
+                          className="__line-no"
+                          key={`number-${i.toString()}`}
+                        >
+                          {i + 1}
+                        </LineNumber>
+                      </div>
+                    )
+                  })}
+                </LineNumbers>
+                <Lines>
+                  {tokens.map((line, i) => {
+                    const lineProps = getLineProps({ line, key: i })
+                    if (highlightedLines.indexOf(i + 1) > -1) {
+                      lineProps.highlighted = true
+                    }
+                    return (
+                      <Line {...lineProps} key={i.toString()}>
+                        <LineContent key={`content-${i.toString()}`}>
+                          {line.map((token, key) => {
+                            const tokenProps = getTokenProps({ token, key })
+                            if (highlightedLines.indexOf(i + 1) > -1) {
+                              tokenProps.highlighted = true
+                            }
+                            return <Token {...tokenProps} key={`${i}-${key}`} />
+                          })}
+                        </LineContent>
+                      </Line>
+                    )
+                  })}
+                </Lines>
+              </Code>
             </Pre>
           )
         }}
