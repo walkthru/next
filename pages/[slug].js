c9efdfcdfe93603222cfa787c4e6266176b5e117
Anthony Gore <anthony@vuejsdevelopers.com>
2022-07-20 09:21:16 +1000

    Updating to 0.2.0 beta 4

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,33 +1,43 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { WalkThru } from '@walkthru/react'
 import Layout from '../components/Layout'
 import NoSSR from '../components/NoSSR'
 
-function Slug({ data }) {
-  const { config } = data
-  const router = useRouter()
-  const segments = router.asPath.split('#')
-  let step = segments[1] ? segments[1] : config.steps[0]
-  if (config.steps.find((slug) => slug === step) === undefined) {
-    step = config.steps[0]
+const instructionsStyle = `
+  color: rgb(55, 65, 81);
+  font-size: 14px;
+  line-height: 24px;
+  font-family: ui-sans-serif, system-ui, -apple-system, "system-ui", "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  
+  li {
+    margin: 3px 0;
   }
-  const [tutorialSlug, setTutorialSlug] = useState(segments[0].substring(1))
-  const [stepSlug, setStepSlug] = useState(step)
+  
+  a[target="_blank"]:after {
+    content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==);
+    margin: 0 1px 0 4px;
+  }
+  
+  code:not([class*=language-]) {
+    color: rgb(249, 38, 114);
+    font-weight: 400;
+    font-size: .875em;
+    padding: 1px 3px;
+    background-color: #f3f4f6;
+    border-radius: 0.25rem;
+  }
+
+  code:not([class*=language-]):before,
+  code:not([class*=language-]):after {
+    content: ''
+  }
+`
+
+function Slug({ data, slug }) {
+  const { config } = data
   useEffect(() => {
     document.title = config.title
   }, [config])
-  useEffect(() => {
-    const onHashChangeStart = ({ newURL }) => {
-      const segments = newURL.split('#')
-      setTutorialSlug(segments[0])
-      setStepSlug(segments[1] ? segments[1] : config.steps[0])
-    }
-    window.addEventListener('hashchange', onHashChangeStart)
-    return () => {
-      window.removeEventListener('hashchange', onHashChangeStart)
-    }
-  }, [router.events, config])
   return (
     <Layout>
       <div
@@ -37,11 +47,11 @@ function Slug({ data }) {
         <NoSSR>
           <WalkThru
             data={data}
-            tutorialSlug={tutorialSlug}
-            stepSlug={stepSlug}
-            classes={{
-              instructions: 'prose prose-sm max-w-none',
-            }}
+            slug={slug}
+            instructionsStyle={(base) => `
+              ${base}
+              ${instructionsStyle}
+            `}
           />
         </NoSSR>
       </div>
@@ -71,8 +81,9 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const githubToken = process.env.GH_PAT
+  const data = await getData({ githubToken })
   return {
-    props: { data: await getData(params.slug, githubToken) },
+    props: data.find((item) => item.slug === params.slug),
   }
 }
 
